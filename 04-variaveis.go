cb6c23fd9ba0a0f179c227dc593ac9f0803bd39a
Felipe Martins <felipemjesus@outlook.com>
2020-08-19 21:55:01 -0300

    Aula sobre tipos de variaveis

---
+++ 04-variaveis.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var inteiro int = 10
+	var _string string = "Tipo string"
+	var semTipo = "Tipo não definido, mas é string"
+
+	decimal := 10.5
+	booleano := true
+
+	fmt.Println(inteiro)
+	fmt.Println(_string)
+	fmt.Println(semTipo)
+	fmt.Println(decimal)
+	fmt.Println(booleano)
+}
