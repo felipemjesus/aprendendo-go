33345a5925b4737adec6959968a37e681cded4c8
Felipe Martins <felipemjesus@outlook.com>
2020-08-19 22:22:22 -0300

    Aula sobre comentarios

---
+++ 07-comentarios.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	//comentário de uma linha
+	fmt.Println("Aula sobre comentário")
+
+	/*
+		comentário 
+		de
+		várias
+		linhas
+	*/
+	fmt.Println("Mais um comentário")
+}
