218607b126b1b7a661385753f15210374e17c23f
Felipe Martins <felipemjesus@outlook.com>
2020-08-21 20:34:57 +0000

    Aula sobre funções

---
+++ 15-funcoes.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	dadosPessoais()
+	dadosPessoais()
+	dadosPessoais()
+}
+
+func dadosPessoais() {
+	fmt.Println("Nome: Felipe Martins")
+	fmt.Println("Idade: 29")
+	fmt.Println("Nacionalidade: Brasileiro")
+}
