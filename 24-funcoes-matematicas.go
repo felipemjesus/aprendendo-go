f29b63ac02efba11674c94aaad278ba43f808069
Felipe Martins <felipemjesus@outlook.com>
2020-08-22 13:31:16 +0000

    Aula sobre funções matematicas

---
+++ 24-funcoes-matematicas.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("Resultado da função Sin():", math.Sin(10.5))
+	fmt.Println("Resultado da função Cos():", math.Cos(10.5))
+	fmt.Println("Resultado da função Tan():", math.Tan(10.5))
+	fmt.Println("Resultado da função Floor():", math.Floor(10.5))
+	fmt.Println("Resultado da função Ceil():", math.Ceil(10.5))
+	fmt.Println("Resultado da função Abs():", math.Abs(-10.5))
+	fmt.Println("Resultado da função Sqrt():", math.Sqrt(10.5))
+	fmt.Println("Resultado da função Pow():", math.Pow(2, 5))
+}
